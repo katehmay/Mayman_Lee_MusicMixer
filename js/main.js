7abf5995c6a44f90bbb1e1c29bfdc7d9298bbdd9
Stella <stella@MacBook-Pro.local>
2019-06-18 00:31:33 -0400

    drag/drop

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
   // also called a lambda, if you're into nerd speak
   console.log('working!');
 
-
-  const dropAreas = document.querySelectorAll('.drop-zone');
-            synths = document.querySelectorAll('#box1'),
+  const dropArea = document.querySelector('#droparea'),
+            dropZones = document.querySelectorAll('.drop-zone'),
+            synths = document.querySelector('#box1'),
             mainVocals = document.querySelectorAll('#box2'),
-            supportVocals = document.querySelectorAll('#box3');
+            supportVocals = document.querySelector('#box3'),
+            guitars = document.querySelector('#box4');
 
   let draggablePieces = document.querySelectorAll("img");
 
@@ -20,24 +21,24 @@
 
   draggablePieces.forEach(piece => {
       piece.addEventListener("dragstart", function(e) {
-        console.log('drag!');
+        console.log('dragging..!');
 
         e.dataTransfer.setData("text/plain", this.id);
     });
   });
 
-  dropAreas.forEach(area => {
-    area.addEventListener("dragover", function(e) {
-        e.preventDefault();
-        console.log('drag is working');
+  dropZones.forEach(zone => {
+       zone.addEventListener("dragover", function(e) {
+           e.preventDefault();
+           console.log('drag is working');
     });
 
-    area.addEventListener("drop", function(ev) {
-        e.preventDefault();
-        console.log('drop is working!');
+      zone.addEventListener("drop", function(e) {
+          e.preventDefault();
+          console.log('drop is working!');
 
-      let dragElement = e.dataTranfer.getData("text/plain");
-      console.log('you drgged: ', dragElement);
+          let dragElement = e.dataTransfer.getData("text/plain");
+          console.log('you drgged: ', dragElement);
 
       e.target.appendChild(document.querySelector(`#${dragElement}`));
     
